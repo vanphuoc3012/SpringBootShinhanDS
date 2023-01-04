27fafd74b32291552561cdde312111f9e9e322f7
Phuoc Ho <vanphuoc3012@gmail.com>
2023-01-04 22:06:11 +0700

    first commit

---
+++ src/main/java/com/likelion/springbootshinhands/SpringBootShinhanDsApplication.java
@@ -0,0 +1,13 @@
+package com.likelion.springbootshinhands;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootShinhanDsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootShinhanDsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/likelion/springbootshinhands/SpringBootShinhanDsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.likelion.springbootshinhands;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootShinhanDsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
